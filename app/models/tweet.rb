2eac15742c8a2305bee45e80d7384f654bf1b449
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-05-03 15:12:43 +0900

    Active_Hash導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,7 +6,11 @@ class Tweet < ApplicationRecord
   validates :phone, presence: true
   validates :image, presence: true
   validates :phone, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
+  validates :place_area_id, numericality: { other_than: 1 }
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :place_area
 end
