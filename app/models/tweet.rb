0a2fd610829323f06b64668f791766fc4a338dc9
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-05-03 12:48:15 +0900

    バリデーションとルーティングの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
+
+  validates :store_name, presence: true
+  validates :store_type, presence: true
+  validates :information, presence: true
+  validates :phone, presence: true
+  validates :image, presence: true
+
   belongs_to :user
   has_one_attached :image
 end
