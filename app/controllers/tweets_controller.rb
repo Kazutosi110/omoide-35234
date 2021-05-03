02fc3d0d73773efe3d8a8cdb4c43c7895e55deca
Kazutosi110 <81143376+Kazutosi110@users.noreply.github.com>
2021-05-03 15:56:47 +0900

    投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,10 +4,20 @@ class TweetsController < ApplicationController
   end
 
   def new
-    
+    @tweet = Tweet.new
   end
 
   def create
-    
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:store_name, :store_type, :information, :place_area_id, :phone, :image).merge(user_id: current_user.id)
   end
 end
